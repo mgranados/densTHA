523d3ac1f0e2a686d6b2fa58ae97dfb15d33a995
Martín Granados García <mgranados@users.noreply.github.com>
2021-07-20 13:54:41 -0500

    Update settings.py

--- densityTHA/settings.py
+++ densityTHA/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=cc%=q(8*09fk-8ce$s-cit-)y9!)ck!mov(5np_i%0g4z#8po'
+SECRET_KEY = 'secretsecret1234'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
